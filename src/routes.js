e27fb809a92701b0a9e768251d6c9e793ee8d625
akkgr <akkgr@users.noreply.github.com>
2017-12-17 20:11:20 +0200

    fix

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import {
     Router
 } from 'express'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const api = db => {
     const api = Router();
+
     api.get('/', (req, res) => {
         db.collection('buildings').find({}).toArray(function (err, docs) {
             if (err) {
@@ -14,6 +17,37 @@ const api = db => {
             }
         });
     });
+
+    api.get('/login', (req, res) => {
+        db.collection('users').findOne({
+            username: req.body.username
+        }).toArray(function (err, user) {
+            if (err) {
+                console.log(err);
+                res.error(err);
+            } else {
+                if (!user) {
+                    res.status(401).json({
+                        message: 'Authentication failed. User not found.'
+                    });
+                } else if (user) {
+                    if (!bcrypt.compareSync(req.body.password, user.password)) {
+                        res.status(401).json({
+                            message: 'Authentication failed. Wrong password.'
+                        });
+                    } else {
+                        return res.json({
+                            token: jwt.sign({
+                                email: user.email,
+                                fullName: user.fullName,
+                                _id: user._id
+                            }, 'RESTFULAPIs')
+                        });
+                    }
+                }
+            }
+        });
+    })
 
     return api
 }
