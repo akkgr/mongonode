7f47ac14f77ddb8da82cea0480f0272a0d6b3eb5
akkgr <akkgr@users.noreply.github.com>
2018-01-15 12:47:39 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,48 +1,58 @@
-import http from 'http';
-import express from 'express'
-import morgan from 'morgan';
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import compress from 'compression'
-import helmet from 'helmet';
-import router from './routes'
-import {
-  MongoClient
-} from 'mongodb'
-import 'babel-polyfill';
-import eaa from 'express-async-await'
+import http from "http";
+import express from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cors from "cors";
+import compress from "compression";
+import helmet from "helmet";
+import router from "./routes";
+import { MongoClient } from "mongodb";
+import "babel-polyfill";
+import eaa from "express-async-await";
+import ws from "socket.io";
 
-const url = 'mongodb://admin:Adm.123@ds243085.mlab.com:43085/estiag';
-const dbName = 'estiag'
+const url = "mongodb://admin:Adm.123@ds243085.mlab.com:43085/estiag";
+const dbName = "estiag";
 const port = process.env.port || 3000;
 const app = eaa(express());
 app.server = http.createServer(app);
-app.use(morgan('dev'));
-app.use(compress())
-app.use(helmet())
+app.use(morgan("dev"));
+app.use(compress());
+app.use(helmet());
 app.use(cors());
-app.use(bodyParser.json({
-  limit: "10mb"
-}));
+app.use(
+  bodyParser.json({
+    limit: "10mb"
+  })
+);
 
-const init = async() => {
+const io = ws(app.server);
+
+io.on("connection", function(socket) {
+  console.log("a user connected");
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+});
+
+const init = async () => {
   try {
-    const client = await MongoClient.connect(url)
-    const db = client.db(dbName)
-    app.use('/', router(db))
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    app.use("/", router(db));
     app.use((err, req, res, next) => {
-      console.error(err)
-      res.status(500).send("Something broke!!")
-    })
+      console.error(err);
+      res.status(500).send("Something broke!!");
+    });
     app.server.listen(port, () => {
-      console.info(`server started on port ${port}`)
-    })
+      console.info(`server started on port ${port}`);
+    });
   } catch (err) {
-    console.error('Failed to make database connection!');
+    console.error("Failed to make database connection!");
     console.error(err);
   }
-}
+};
 
-init()
+init();
 
 export default app;
--- src/routes.js
+++ src/routes.js
@@ -1,51 +1,60 @@
-import {
-    Router
-} from 'express'
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import eaa from 'express-async-await'
+import { Router } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import eaa from "express-async-await";
 
 const router = db => {
-    const api = eaa(Router());
+  const api = eaa(Router());
 
-    api.get('/:collection', async(req, res) => {
-        const collection = req.params.collection
-        const docs = await db.collection(collection).find({}).toArray()
-        res.json(docs)
-    });
+  api.get("/:collection", async (req, res) => {
+    const collection = req.params.collection;
+    try {
+      const docs = await db
+        .collection(collection)
+        .find({})
+        .toArray();
+      res.json(docs);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
-    api.get('/login', (req, res) => {
-        db.collection('users').findOne({
-            username: req.body.username
-        }).toArray(function (err, user) {
-            if (err) {
-                console.log(err);
-                res.error(err);
-            } else {
-                if (!user) {
-                    res.status(401).json({
-                        message: 'Authentication failed. User not found.'
-                    });
-                } else if (user) {
-                    if (!bcrypt.compareSync(req.body.password, user.password)) {
-                        res.status(401).json({
-                            message: 'Authentication failed. Wrong password.'
-                        });
-                    } else {
-                        return res.json({
-                            token: jwt.sign({
-                                email: user.email,
-                                fullName: user.fullName,
-                                _id: user._id
-                            }, 'RESTFULAPIs')
-                        });
-                    }
-                }
-            }
+  api.post("/login", async (req, res) => {
+    try {
+      const users = await db
+        .collection("users")
+        .find({
+          username: req.body.username
+        })
+        .toArray();
+      if (!users || users.length != 1) {
+        res.status(401).json({
+          message: "Authentication failed. User not found."
         });
-    })
+        return;
+      }
+      if (!bcrypt.compareSync(req.body.password, users[0].password)) {
+        res.status(401).json({
+          message: "Authentication failed. Wrong password."
+        });
+        return;
+      }
+      res.json({
+        token: jwt.sign(
+          {
+            email: users[0].email,
+            fullName: users[0].fullName,
+            _id: users[0]._id
+          },
+          "RESTFULAPIs"
+        )
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
-    return api
-}
+  return api;
+};
 
-export default router
+export default router;
