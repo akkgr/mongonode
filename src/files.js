e09cd0978f7f6785c8380206d417eb4fde96949b
akkgr <akkgr@users.noreply.github.com>
2018-05-16 14:38:24 +0300

    fix

--- src/files.js
+++ src/files.js
@@ -1,30 +1,41 @@
-import formidable from "formidable";
+import formidable from 'formidable'
+import { GridFSBucket } from 'mongodb'
+import fs from 'fs'
 
 const files = db => {
-  const api = {};
+  const api = {}
 
   api.upload = async (req, res) => {
     try {
-      var form = new formidable.IncomingForm();
-      form.uploadDir = "./uploads";
-      form.on("progress", function(recv, total) {
+      var form = new formidable.IncomingForm()
+      form.uploadDir = './uploads'
+      form.on('progress', function(recv, total) {
+        // File size check ( <= 30Mb )
         if (total > 30 * 1024 * 1024) {
-          throw new Error("file too large");
+          throw new Error('file too large')
         }
-      });
-      form.on("file", function(name, file) {
-        // if you need to process uploading files one by one, you can do it here
-        // and do nothing in the form.parse  callback function
-      });
+      })
+      form.on('file', function(name, file) {
+        var bucket = new GridFSBucket(db)
+        fs
+          .createReadStream('./uploads/' + name)
+          .pipe(bucket.openUploadStream(name))
+          .on('error', function(error) {
+            res.status(500).end()
+          })
+          .on('finish', function() {
+            res.status(200).end()
+          })
+      })
       form.parse(req, (err, fields, files) => {
-        res.status(200).end();
-      });
+        res.status(200).end()
+      })
     } catch (err) {
-      res.status(500).send(err.message);
+      res.status(500).send(err.message)
     }
-  };
+  }
 
-  return api;
-};
+  return api
+}
 
-export default files;
+export default files
